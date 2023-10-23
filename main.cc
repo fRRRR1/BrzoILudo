ce0b3724514cfbc8fcfde592ac7bf8bf0ac2c06a
arsa <marsalaca@gmail.com>
2023-10-23 02:30:51 +0200

    updated config for database

--- main.cc
+++ main.cc
@@ -6,6 +6,26 @@ int main() {
     //drogon::app().loadConfigFile("../config.json");
     //drogon::app().loadConfigFile("../config.yaml");
     //Run HTTP framework,the method will block in the internal event loop
+
+    // example of using orm to query database
+    // app().registerHandler("/arsa", [](HttpRequestPtr req) -> Task<HttpResponsePtr>
+    // {
+    //     auto clientPtr = drogon::app().getDbClient();
+    //     clientPtr->execSqlAsync("select * from posts",
+    //     [](const drogon::orm::Result &result) {
+    //         std::cout << result.size() << " rows selected!" << std::endl;
+    //         int i = 0;
+    //         for (auto row : result)
+    //         {
+    //             std::cout << i++ << ": user name is " << row["user_name"].as<std::string>() << std::endl;
+    //         }
+    //     },
+    //     [](const drogon::orm::DrogonDbException &e) {
+    //         std::cerr << "error:" << e.base().what() << std::endl;
+    //     },
+    //     "default");
+    // });
+
     LOG_INFO << "Server running on 127.0.0.1:5555";
     drogon::app().run();
     return 0;
